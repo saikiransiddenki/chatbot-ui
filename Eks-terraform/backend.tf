8613f888414969c9bae16424ae760481ab477991
saikiransiddenki <161606638+saikiransiddenki@users.noreply.github.com>
2024-04-19 09:00:38 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sai-mrcloudbook0603456777" # Replace with your actual S3 bucket name
+    bucket = "sai-mrcloudbook7890754356777" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
