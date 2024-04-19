2dffb62100db3df5ef3dda321c17976d547e4ef4
saikiransiddenki <161606638+saikiransiddenki@users.noreply.github.com>
2024-04-19 09:16:46 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -10,4 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "terraform"
 }
