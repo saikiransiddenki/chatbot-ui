9d406cc74275ec378630ae7701444e3b1c7e90e8
saikiransiddenki <161606638+saikiransiddenki@users.noreply.github.com>
2024-04-19 10:27:12 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -105,7 +105,7 @@ resource "aws_eks_node_group" "example" {
     min_size     = 1
   }
 
- instance_types = ["t2.large"]
+ instance_types = ["t2.medium"]
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
